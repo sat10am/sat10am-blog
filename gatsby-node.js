e27ab72b99132ec889dd5cd66b463df62abc6633
Hosung Lim <holnet1026@gmail.com>
2019-05-01 17:35:43 +0900

    feat: ⚡️ implement create post detail page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,32 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path");
 
-// You can delete this file if you're not using it
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const postTemplate = path.resolve(`src/templates/PostTemplate.jsx`);
+  return graphql(`
+    {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
+        context: {},
+      });
+    });
+  });
+};
