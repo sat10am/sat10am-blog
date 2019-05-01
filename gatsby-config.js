623c8a286312cc5948a0ebf400499e8612e30301
Hosung Lim <holnet1026@gmail.com>
2019-05-01 16:39:18 +0900

    chore: 📦 gatsby config filesystem & transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SAT10AM`,
+    description: `A Study Blog for SAT10AM`,
+    author: `@y0c`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/contents`,
+        name: "markdown-pages",
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
-}
+};
