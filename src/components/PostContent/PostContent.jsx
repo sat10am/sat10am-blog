40a126521a8300f74a3d29fecb439ae48b6e7238
Hosung Lim <holnet1026@gmail.com>
2019-05-13 22:42:43 +0900

    refactor: 🛠 re-style PostContent
    * gatsby-highlight
    * apply github markdown
    * show source language

--- src/components/PostContent/PostContent.jsx
+++ src/components/PostContent/PostContent.jsx
@@ -1,72 +1,77 @@
-import React from "react";
-import styled from "styled-components";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import 'github-markdown-css';
 
 const ContentWrapper = styled.div`
-  * {
-    font-family: "Noto Sans KR";
-  }
-  p {
-    margin-bottom: 15px;
-    line-height: 24px;
-    color: #4b4b4b;
+  code.language-text {
+    background: #eee;
+    color: #c77532;
   }
 
-  ul {
-    list-style: square;
-    padding-left: 20px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    color: #4b4b4b;
-    li {
-      margin-bottom: 8px;
-    }
-  }
-
+  ul,
   ol {
-    list-style: decimal;
-    padding-left: 30px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    color: #4b4b4b;
-    li {
-      margin-bottom: 8px;
-    }
+    list-style: initial;
   }
 
-  p {
+  :not(pre) > code[class*='language-'],
+  pre[class*='language-'] {
+    background: #171717;
+  }
+
+  .gatsby-highlight {
+    position: relative;
+    box-sizing: border-box;
+
+    pre {
+      padding-top: 30px;
+    }
+
     code.language-text {
-      background: #eee;
-      color: #4b4b4b;
-      padding: 2px;
+      background: #2b2b2b;
+      color: #a9b7c6;
+    }
+    .line-numbers-rows {
+      padding-left: 3px;
     }
   }
 
-  a {
-    color: skyblue;
+  .gatsby-highlight::before {
+    content: attr(data-language);
+    display: block;
+    position: absolute;
+    top: 0px;
+    right: 20px;
+    height: 20px;
+    font-size: 12px;
+    text-transform: uppercase;
+    border-bottom-left-radius: 3px;
+    border-bottom-right-radius: 3px;
+    padding: 1px 5px;
+    background: #ffc66d;
+    color: #c77532;
+    font-weight: 500;
+    z-index: 9;
   }
 
-  h1 {
-    font-size: 32px;
-  }
-  h2 {
-    font-size: 26px;
-  }
-  h3 {
-    font-size: 18px;
+  .gatsby-highlight pre[class*='language-'].line-numbers {
+    padding-left: 2.8em;
   }
 
-  h1,
-  h2 {
-    color: #4b4b4b;
-    padding-bottom: 10px;
-    border-bottom: 1px solid #ddd;
-    margin-bottom: 5px;
+  .gatsby-highlight pre[class*='language-'].line-numbers {
+    padding: 0;
+    padding-left: 2.8em;
+    overflow: initial;
   }
 `;
 
 const PostContent = ({ html }) => {
-  return <ContentWrapper dangerouslySetInnerHTML={{ __html: html }} />;
+  return (
+    <ContentWrapper
+      className='markdown-body'
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  );
 };
 
 PostContent.propTypes = {
