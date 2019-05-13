d0e1a24420a9f3b4ce8d3e080b7153b550c2150d
Hosung Lim <holnet1026@gmail.com>
2019-05-13 22:43:44 +0900

    refactor: 🛠 change source theme & add prism plugin
    * dacular theme
    * show language
    * line-highlight
    * line number

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,5 @@
-require("prism-themes/themes/prism-duotone-dark.css");
+require('prism-themes/themes/prism-darcula.css');
+require('prismjs/plugins/show-language/prism-show-language');
+require('prismjs/plugins/line-highlight/prism-line-highlight.css');
+require('prismjs/plugins/line-highlight/prism-line-highlight');
+require('prismjs/plugins/line-numbers/prism-line-numbers.css');
