647c5cb89453d32b42ed4fb3701ce091b99b7963
Hosung Lim <holnet1026@gmail.com>
2019-05-20 19:33:40 +0900

    refactor: 🛠 change PostProfile contents

--- src/components/PostProfile/PostProfile.jsx
+++ src/components/PostProfile/PostProfile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import TagList from '../TagList';
+import { MdMail, MdMessage } from 'react-icons/md';
+import { GoMarkGithub } from 'react-icons/go';
 
 const ProfileWrapper = styled.div`
   display: flex;
@@ -27,19 +28,52 @@ const ProfileName = styled.h1`
 
 const ProfileDescription = styled.span`
   font-size: 0.8rem;
+  display: inline-flex;
+  align-items: center;
   color: #999;
-  margin-bottom: 10px;
+  margin-bottom: 5px;
+  a {
+    color: #999;
+    text-decoration: none;
+  }
+  svg {
+    color: #4b4b4b;
+    margin-right: 5px;
+  }
 `;
 
 const PostProfile = ({ member }) => (
   <ProfileWrapper>
-    <ProfileImg src={member.profile} alt={member.username} />
+    <ProfileImg src={member.profile.url} alt={member.username} />
     <div>
       <ProfileName>{member.username}</ProfileName>
-      <ProfileDescription>
-        여기는 간단한 소개가 들어갈 예정이에요
-      </ProfileDescription>
-      <TagList tags={member.skill} />
+      <ul>
+        <li>
+          <ProfileDescription>
+            <MdMail />
+            <a href={`mailto:${member.email}`} rel='noreferrer noopener'>
+              {member.email}
+            </a>
+          </ProfileDescription>
+        </li>
+        <li>
+          <ProfileDescription>
+            <GoMarkGithub />
+            <a
+              href={member.githubUrl}
+              target='_blank'
+              rel='noreferrer noopener'>
+              {member.githubUrl}
+            </a>
+          </ProfileDescription>
+        </li>
+        <li>
+          <ProfileDescription>
+            <MdMessage />
+            {member.intro}
+          </ProfileDescription>
+        </li>
+      </ul>
     </div>
   </ProfileWrapper>
 );
@@ -48,7 +82,7 @@ PostProfile.propTypes = {
   member: PropTypes.shape({
     profile: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
-    skill: PropTypes.arrayOf(PropTypes.string).isRequired,
+    intro: PropTypes.string.isRequired,
   }).isRequired,
 };
 
